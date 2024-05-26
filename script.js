0e796d4295b6d953fe01a5c9468375dc79e3cea0
Trigo <trigoyodila1996@gmail.com>
2024-05-26 22:13:22 +0200

    code static html et css

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Get the file input element and the upload button
+/*const imageInput = document.querySelector('.inputImage');
+const uploadButton = document.querySelector('.btn-image');
+const previewImage = document.querySelector('.previewImage');
+
+// ajout de event listener sur le fichier input
+imageInput.addEventListener('change', (event) => {
+
+    event.preventDefault()
+  // recuperer le fichier selectionné
+  const file = event.target.files[0];
+
+  console.log("files ", file)
+
+  // test si le fichier existe
+  if (file) {
+    // créer un url pour le fichier
+    const imageUrl = URL.createObjectURL(file);
+
+    // affichage de l'image
+    previewImage.style.display = 'inline'
+    previewImage.src = imageUrl
+    
+  }
+}); */
