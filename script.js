181d236c1fe11ff3b69344431ddfcb0afc20ba97
Trigo <trigoyodila1996@gmail.com>
2024-05-27 00:19:56 +0200

    correction des bugs

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const bio = document.querySelector('textarea')
 
 const btnCreer = document.querySelector('.creer')
 
+const contactList = document.querySelector('.content-list')
+contactList.classList.add('content-list')
+
 const form = document.querySelector("form")
 
 let imgUrl
@@ -40,7 +43,7 @@ btnCreer.addEventListener('click', (e) => {
     const nom = inputsText[1].value
     const bioText = bio.value
     const sexeValue = genre[genre.selectedIndex].value
-
+   
     // créations des éléments
     const divContact = document.createElement('div')
 
@@ -54,8 +57,41 @@ btnCreer.addEventListener('click', (e) => {
 
     // right
     const divRightContact = document.createElement('div')
+    const divBtnCroix = document.createElement('div')
+    divBtnCroix.classList.add('btnCroix')
+    const imgCroix = document.createElement('img')
+    imgCroix.classList.add('image-croix')
+    imgCroix.src='./images/croix.png'
 
-    
+    divBtnCroix.appendChild(imgCroix)
+
+    const labelPrenom = document.createElement('label')
+    const labelNom = document.createElement('label')
+    labelNom.classList.add('textLabel')
+    labelPrenom.classList.add('textLabel')
+
+    labelPrenom.textContent = prenom
+    labelNom.textContent = nom
+
+    const paragSexe = document.createElement('p')
+    paragSexe.classList.add('textLabel')
+    paragSexe.textContent = sexeValue
+
+    const paragBio = document.createElement('p')
+    paragBio.textContent = bioText
+
+    divRightContact.appendChild(divBtnCroix)
+    divRightContact.appendChild(labelPrenom)
+    divRightContact.appendChild(labelNom)
+    divRightContact.appendChild(paragSexe)
+    divRightContact.appendChild(paragBio)
+
+    divContact.classList.add('contact')
+    divContact.appendChild(divLeftContact)
+    divContact.appendChild(divRightContact)
+
+    contactList.appendChild(divContact)
+ 
 })
 
 
